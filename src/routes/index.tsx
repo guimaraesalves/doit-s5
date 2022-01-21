b2d9f27073a64b51158e6f62c6daf4075b0e8975
guimaraesalves <guimaraesalvesmateus@gmail.com>
2022-01-21 15:35:09 -0300

    feature: base project with chakra ui theme

---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+import { Route, Switch } from "react-router-dom";
+
+import { Login } from "../pages/Login";
+
+export const Routes = () => (
+  <Switch>
+    <Route exact path="/" component={Login} />
+  </Switch>
+);
